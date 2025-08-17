970d97633b333f454d204fbb30b6abad620a99f2
Bryan Ibe <nnaemekabryan@gmail.com>
2025-08-17 18:12:02 +0100

    Add build.gradle.kts for data-core module configuration

---
+++ data-core/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins{
+    id("android-library-convention")
+    id("kotlin-library-convention")
+    id("ksp-convention")
+    kotlin("plugin.serialization")
+}
+
+android{
+    namespace = "com.bryan.egret.data"
+}
+
+
+kotlin{
+    sourceSets{
+        commonMain{
+            dependencies{
+                api(project(":di"))
+                implementation(libs.kotlinx.datetime)
+//                implementation(libs.kotlinx.serialization)
+            }
+        }
+        commonTest{
+            dependencies{
+                implementation(libs.kotlin.test)
+            }
+        }
+    }
+}
+
+dependencies{
+    implementation(project(":di"))
+}
